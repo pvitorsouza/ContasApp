15a9be8c55eda8bdb8be330bf33420213319fdff
pvitorsouza <vitin.7@icloud.com>
2023-07-21 12:04:49 -0300

    feat: Desenvolvimento do dashboard

---
+++ ContasApp.Presentation/Models/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ContasApp.Presentation.Models
+{
+    public class DashboardViewModel
+    {
+        /// <summary>
+        /// Modelo de dados para a página do Dashboard
+        /// </summary>
+        public int? Mes { get; set; }
+        public int? Ano { get; set; }
+    }
+}
